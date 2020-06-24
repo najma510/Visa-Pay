4d71e4251791db1d45435ba3d4b9beeb93dd893a
mikal09 <kumar.mikal@gmail.com>
2020-06-24 19:29:25 +0530

    updated

--- lib/screens/paymentlink_screen.dart
+++ lib/screens/paymentlink_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class PaymentLinkScreen extends StatefulWidget {
   @override
   _PaymentLinkScreenState createState() => _PaymentLinkScreenState();
 }
+
+String link ="https://www.google.co.in/";
 
 class _PaymentLinkScreenState extends State<PaymentLinkScreen> {
   @override
@@ -26,9 +29,77 @@ class _PaymentLinkScreenState extends State<PaymentLinkScreen> {
                 ),
               ),
             ),
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/visa.png'),
+              ),
+            ),
+            SizedBox(height: 40),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      height: 130.0,
+                      child: Image.asset('images/check.png'),
+                    ),
+                    SizedBox(height: 50),
+                    Text(
+                      "Hi Merchnat your link is successfully Created",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontFamily: 'signatra',
+                        fontSize: 22,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      child: Card(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        color: Colors.blueAccent,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            "Click Here To Share Or Copy Your Link",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onTap: () => share(context, link),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Note: Link will expire in next 15 minutes",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+void share(BuildContext context, String link) {
+  final RenderBox box = context.findRenderObject();
+
+  Share.share(
+    link,
+    subject: link,
+    sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size,
+  );
 }
